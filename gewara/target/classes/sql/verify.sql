ac861d753c944630e7963a04c465dc30302594a8
sunder-xie <xieshengrong@live.com>
2017-07-28 17:18:59 +0800

    优化仓库,去除target文件夹

--- gewara/target/classes/sql/verify.sql
+++
@@ -1,3 +0,0 @@
-delete from info_member_action WHERE c_member_id NOT IN (SELECT c_member_id from c_member);
-select g.*,rowid from gym g where g.name like 'test%';
-select g.*,rowid from gymprogram g where g.programname like 'test%';
