1f7892bc7a713998b7ce201f31a9ab17d6a12bbd
sunder-xie <xieshengrong@live.com>
2017-08-03 13:58:08 +0800

    更新

--- gewara/src/main/java/com/gewara/support/GewaLoadEventListener.java
+++ gewara/src/main/java/com/gewara/support/GewaLoadEventListener.java
@@ -1,7 +1,9 @@
 package com.gewara.support;
 
+import org.hibernate.event.internal.DefaultLoadEventListener;
+
 /**
  * Created by sunder on 2017/8/3.
  */
-public class GewaLoadEventListener {
+public class GewaLoadEventListener extends DefaultLoadEventListener{
 }
